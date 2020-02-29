013125d867dbb22fefff3d1797e34956fccaa5b6
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-02-29 11:39:57 -0300

    Criado o modelo de funcionário, com atributos pendentes ainda.

---
+++ src/main/java/cast/vacation/ctrl/model/Funcionario.java
@@ -0,0 +1,50 @@
+package cast.vacation.ctrl.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cd_func")
+	private Long idFuncionario;
+	
+	@NotNull
+	@Column(name = "nm_funcionario")
+	private String nomeFuncionario;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	
+	public Long getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(Long idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+	
+	
+	
+
+	
+}
